f2ded63a114579979890f6b7e8514053cca95e96
sau-skt <saurabhchatterjee18@gmail.com>
2020-04-02 17:33:59 +0530

    Completed

---
+++ app/src/main/java/appdev/com/navigationview/Steps.java
@@ -0,0 +1,9 @@
+package appdev.com.navigationview;
+
+public class Steps {
+    /**
+     * Create menu/drawer_menu
+     * android:checkableBehavior="single" once item is clicked it remains selected
+     *
+     */
+}
